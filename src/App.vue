1ce6c2c5b46f81434ac9c7383f13bb584fb7d0c4
khoinguyen28102002 <khoinguyennguyen914@gmail.com>
2025-09-11 17:53:01 +0700

    Test

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from "vue";
+import HelloWorld from './components/HelloWorld.vue'
+import Button from "primevue/button";
+import Menu from 'primevue/menu';
+import Menubar from 'primevue/menubar';
+import { useRouter } from 'vue-router';
+const items = ref([
+    { label: 'New', icon: 'pi pi-plus' },
+    { label: 'Search', icon: 'pi pi-search' }
+]);
+const menuItems = [
+    {
+        label: 'Library',
+        icon: 'pi pi-book',
+        command: () => router.push('/library')
+    },
+    {
+        label: 'Manage',
+        icon: 'pi pi-cog',
+        command: () => router.push('/manage')
+    }
+];
+</script>
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <div class="card flex justify-center">
+        <Button label="Verify" />
+        <Menubar :model="items" />
+    </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from "@primevue/themes/aura";       //theme
+import "primeicons/primeicons.css";                 //icons
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/library' },
+    { path: '/library', component: GetBooks },
+    { path: '/manage', component: ManageBook }
+  ]
+})
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.use(Aura);
+app.use(router);
+app.mount('#app')
